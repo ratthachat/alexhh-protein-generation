f3ac35cb866402fc916ff9da1127da27a9ebd21e
Ratthachat (Jung) <56621342+ratthachat@users.noreply.github.com>
2022-05-18 08:59:38 +0000

    runnable as TF2.0

--- models/protcnn.py
+++ models/protcnn.py
@@ -3,10 +3,9 @@ import tensorflow as tf
 from functools import partial
 import numpy as np
 
-
 from keras import backend as K
 from keras import losses
-from keras.callbacks import TensorBoard, Callback
+from keras.callbacks import Callback
 from keras.models import Model
 from keras.layers import Input, Lambda
 
@@ -14,6 +13,8 @@ from utils import aa_letters, luxa_seq
 from utils.metrics import aa_acc
 from utils.data_loaders import right_pad, to_one_hot
 from utils.decoding import _decode_ar, _decode_nonar, batch_temp_sample
+from models.encoders import fc_encoder
+from models.decoders import fc_decoder
 
 nchar = len(aa_letters)  # = 21
 
@@ -30,11 +31,10 @@ def luxa_batch_conds(n_samples, solubility_level):
     return target_conds
 
 
-class BaseProtVAE:
-    # Child classes must define a self.E, self.G
-    def __init__(self, n_conditions=0, autoregressive=True,
+class ProtMSAVAE:
+    def __init__(self, n_conditions=0, autoregressive=False,
                  lr=0.001, clipnorm=0., clipvalue=0., metrics=['accuracy', aa_acc],
-                 condition_encoder=True, latent_dim=50, original_dim=504):
+                 condition_encoder=True, latent_dim=10, original_dim=360):
 
         self.n_conditions = n_conditions
         self.condition_encoder = condition_encoder
@@ -42,28 +42,49 @@ class BaseProtVAE:
         self.latent_dim = latent_dim
         self.original_dim = original_dim
 
-        self.S = sampler(latent_dim, epsilon_std=1.)
+        encoder_kwargs={'encoder_hidden': [256, 256],
+                        'encoder_dropout': [0., 0.]}
+        decoder_kwargs={'decoder_hidden': [256,256],
+                        'decoder_dropout': [0.,0.]}
+        self.activation='relu'
+        self.alphabet_size=21
 
-        prot = self.E.inputs[0]
-        encoder_inp = [prot]
-        vae_inp = [prot]
+        self.E = fc_encoder(self.original_dim, self.latent_dim,
+                            n_conditions=self.n_conditions,
+                            alphabet_size=self.alphabet_size,
+                            activation=self.activation,
+                            **encoder_kwargs)
+        self.G = fc_decoder(self.latent_dim, self.original_dim,
+                            n_conditions=self.n_conditions,
+                            alphabet_size=self.alphabet_size,
+                            activation=self.activation,
+                            **decoder_kwargs)
+
+
+        self.S = sampler(latent_dim, epsilon_std=1.)
+        encoder_inp = Input(shape=(self.original_dim, self.alphabet_size))
+        vae_inp = encoder_inp
+
+        # prot = self.E.inputs[0]
+        # encoder_inp = [prot]
+        # vae_inp = [prot]
         
-        if n_conditions>0:
-            conditions = Input((n_conditions,))
-            vae_inp.append(conditions)
-            if condition_encoder:
-                encoder_inp.append(conditions)
+        # if n_conditions>0:
+        #     conditions = Input((n_conditions,))
+        #     vae_inp.append(conditions)
+        #     if condition_encoder:
+        #         encoder_inp.append(conditions)
 
         z_mean, z_var = self.E(encoder_inp)
         z = self.S([z_mean, z_var])  # sampler
         self.stochastic_E = Model(inputs=encoder_inp, outputs=[z_mean, z_var, z])
         
-        decoder_inp = [z]
-        if n_conditions > 0:
-            decoder_inp.append(conditions)
+        decoder_inp = z#[z]
+        # if n_conditions > 0:
+        #     decoder_inp.append(conditions)
 
-        if autoregressive:
-            decoder_inp.append(prot)
+        # if autoregressive:
+        #     decoder_inp.append(prot)
 
         decoded = self.G(decoder_inp)
         
@@ -72,8 +93,8 @@ class BaseProtVAE:
         def xent_loss(x, x_d_m):
             return K.sum(losses.categorical_crossentropy(x, x_d_m), -1)
 
-        def kl_loss(x, x_d_m):
-            return - 0.5 * K.sum(1 + K.log(z_var + 1e-8) - K.square(z_mean) - z_var, axis=-1)
+        def kl_loss(x, x_d_m): # very adhoc definition!! 
+            return - 0.5 #* K.sum(1 + K.log(z_var + 1e-8) - K.square(z_mean) - z_var, axis=-1)
 
         def vae_loss(x, x_d_m):
             return xent_loss(x, x_d_m) + kl_loss(x, x_d_m)
@@ -81,8 +102,10 @@ class BaseProtVAE:
         log_metrics = metrics + [xent_loss, kl_loss, vae_loss]
 
         print('Learning rate ', lr)
-        self.VAE.compile(loss=vae_loss, optimizer="adam",
-                         metrics=log_metrics)
+        self.VAE.compile(loss=vae_loss, 
+                         optimizer="adam",
+                         metrics=log_metrics
+                         )
         # self.VAE.compile(loss=vae_loss, optimizer=Adam(lr=lr, clipnorm=clipnorm, clipvalue=clipvalue),
         #                  metrics=log_metrics)
         
@@ -122,7 +145,7 @@ class BaseProtVAE:
             return _decode_nonar(self.G, z, remove_gaps=remove_gaps, conditions=conditions)
 
     def generate_variants_luxA(self, num_samples, posterior_var_scale=1., temperature=0.,
-                               solubility_level=None):
+                               solubility_level=None,remove_gaps=True):
 
         luxa_oh = to_one_hot(right_pad([luxa_seq], self.E.input_shape[1]))
         luxa_oh = np.repeat(luxa_oh, num_samples, axis=0)
@@ -139,5 +162,5 @@ class BaseProtVAE:
         if temperature > 0:
             sample_func = partial(batch_temp_sample, temperature=temperature)
         target_conds = None if solubility_level is None else luxa_batch_conds(num_samples, solubility_level)
-        return self.decode(luxa_z, remove_gaps=True, sample_func=sample_func,
+        return self.decode(luxa_z, remove_gaps=remove_gaps, sample_func=sample_func,
                            conditions=target_conds)
